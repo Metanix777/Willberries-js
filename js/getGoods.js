7cee60e2c0c3bbb51c019f224ea8c7a4e53bca3d
Ivan work <shukalo.96@gmail.com>
2022-03-01 16:47:19 +0300

    fetch: day 3

--- js/getGoods.js
+++ js/getGoods.js
@@ -2,7 +2,30 @@ const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link')
 
     const renderGoods = (goods) => {
-        console.log(goods)
+        const goodsContainer = document.querySelector('.long-goods-list')
+
+        goodsContainer.innerHTML = ''
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-sm-6')
+
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
+            `
+
+            goodsContainer.append(goodBlock)
+        })
     }
 
     const getData = (value, category) => {
@@ -11,9 +34,8 @@ const getGoods = () => {
             .then((data) => {
                 const array = category ? data.filter((item) => item[category] === value) : data
                 localStorage.setItem('goods', JSON.stringify(array))
-
-                if (window.location.pathname !== 'goods.html') {
-                    window.location.href = 'goods.html'
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html'
                 } else {
                     renderGoods(array)
                 }
@@ -29,7 +51,7 @@ const getGoods = () => {
         })
     })
 
-    if (localStorage.getItem('goods') && window.location.pathname === 'goods.html') {
+    if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
         renderGoods(JSON.parse(localStorage.getItem('goods')))
     }
 }
